247bed915bcb2dbec8aa945667c9b5e9acead021
w-modz <46196436+w-modz@users.noreply.github.com>
2026-09-23 16:12:30 +0200

    Implement Exercise 2

--- Lab2/Exercise2/Exercise2.hs
+++ Lab2/Exercise2/Exercise2.hs
@@ -1,7 +1,80 @@
-module Exercise1 where
+module Exercise2 where
 
 import Test.QuickCheck
+import SetOrd
+import Exercise1 (genSet, genSetQuickCheck)
+import Control.Monad (replicateM)
+
+-- run with:
+-- runghc --ghc-arg=-i..;../Exercise1 Exercise2.hs +RTS -M512M -RTS
+
+-- Operator for union of two sets
+setUnion :: (Ord a) => Set a -> Set a -> Set a
+setUnion (Set xs) (Set ys) = list2set (xs ++ ys)
+
+-- Operator for intersection of two sets
+setIntersection :: (Ord a) => Set a -> Set a -> Set a
+setIntersection (Set xs) (Set ys) = list2set [x | x <- xs, x `elem` ys]
+
+-- Operator for difference of two sets
+setDifference :: (Ord a) => Set a -> Set a -> Set a
+setDifference (Set xs) (Set ys) = list2set [x | x <- xs, x `notElem` ys]
+
+-- Property to test that the union of two sets is commutative
+propUnionCummutative :: Set Int -> Set Int -> Bool
+propUnionCummutative set1 set2 = setUnion set1 set2 == setUnion set2 set1
+
+-- Property to test that the intersection of two sets is commutative
+propIntersectionCummutative :: Set Int -> Set Int -> Bool
+propIntersectionCummutative set1 set2 = setIntersection set1 set2 == setIntersection set2 set1
+
+-- Property to test that the difference of two sets is not commutative
+propDifferenceNonCummutative :: Set Int -> Set Int -> Property
+propDifferenceNonCummutative set1 set2 =
+    condition ==> differenceIsDifferent
+  where
+    -- Condition to ensure that the sets are not equal and not empty
+    condition =
+        not (isEmpty set1)
+        && not (isEmpty set2)
+        && set1 /= set2
+        && not (subSet set1 set2)
+        && not (subSet set2 set1)
+
+    differenceIsDifferent =
+        setDifference set1 set2 /= setDifference set2 set1
+
+-- Property to test that the union of a set with itself is idempotent
+propIndempotentUnion :: Set Int -> Bool
+propIndempotentUnion set = setUnion set set == set
+
+-- Property to test that the intersection of a set with itself is idempotent
+propIndempotentIntersection :: Set Int -> Bool
+propIndempotentIntersection set = setIntersection set set == set
+
+-- Property to test that the difference of a set with itself is empty
+propDifferenceSelf :: Set Int -> Bool
+propDifferenceSelf set =
+    setDifference set set == emptySet
 
 main ::  IO ()
 main = do
-    putStrLn "1"
+    -- 100 random tests
+    results <- replicateM 100 $ do
+        set1 <- genSet 10
+        set2 <- genSet 10
+        return $ setUnion set1 set2 == setUnion set2 set1
+            && setIntersection set1 set2 == setIntersection set2 set1
+            && setDifference set1 set2 /= setDifference set2 set1
+            && setUnion set1 set1 == set1
+            && setIntersection set1 set1 == set1
+            && setDifference set1 set1 == emptySet
+    print ("Tests passed:" ++ show (and results))
+
+    -- QuickCheck tests
+    quickCheck propUnionCummutative
+    quickCheck propIntersectionCummutative
+    quickCheck propDifferenceNonCummutative
+    quickCheck propIndempotentUnion
+    quickCheck propIndempotentIntersection
+    quickCheck propDifferenceSelf
